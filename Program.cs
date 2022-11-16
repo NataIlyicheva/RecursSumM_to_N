5532748360fa5804faebd13012d0462fa3d631b7
Natali <n7483205@ya.ru>
2022-11-16 11:49:45 +0700

    task66

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных 
+элементов в промежутке от M до N. Выполнить с помощью рекурсии. */
+
+int GetSum(int m, int n)
+{   
+    int sum;
+     
+    if (m >= n)
+       return m;
+    else
+    {
+        return sum = n + GetSum(m, n - 1);
+    }
+    
+}
+Console.WriteLine(GetSum(1, 5));
+
+  
